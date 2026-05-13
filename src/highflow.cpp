114703e0a9f449820ca06f315bf075053f7d391e
wey-flefy <FFl@weycor.de>
2026-05-13 12:34:08 +0200

    Testhighflow.cpp angepasst, Fehler injeziert, um die Fehlererkennung zu testen. main.cpp hinzugefügt, um die Kompilierung zu ermöglichen. Highflow.h angepasst, um die Funktion highflow zu deklarieren.

--- src/highflow.cpp
+++ src/highflow.cpp
@@ -4,10 +4,10 @@
 int highflow (int rpm) {
     
     if (rpm < rpm_LL) {
-        return 0; // Durchflussrate zu hoch
+        return 0; 
     }
     if (rpm > rpm_UL) {
-        return 10; // Durchflussrate zu hoch
+        return 10; 
     }
     return (rpm - rpm_LL) * 100 / (rpm_UL - rpm_LL); // Berechnung der Durchflussrate in Prozent    
 }
--- tests/TEST_highflow.cpp
+++ tests/TEST_highflow.cpp
@@ -4,17 +4,18 @@
 int main() {
     int rpm = 0; // Beispielwert für Drehzahl
     int ErrorCounter = 0; // Zähler für Fehler
+    int testcounter = 20; // Zähler für Tests
 
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < testcounter; ++i) {
          rpm += 200; // Erhöhe die Drehzahl in jedem Schritt
          std::cout << "Aktuelle Drehzahl: " << rpm << std::endl;
          int gauge_position = highflow(rpm);
          std::cout << "gauge_position: " << gauge_position << "%" << std::endl;
-         if (gauge_position == 0 && rpm >= rpm_LL) {
+         if (gauge_position == 0 && rpm >= rpm_LL || gauge_position != 100 && rpm >= rpm_UL) {
              std::cout << "FAILED: gauge_position zu niedrig, aber Drehzahl ist über dem unteren Grenzwert!" << std::endl;
              ErrorCounter++; // Fehlerzähler erhöhen, wenn die gauge_position zu hoch ist
          }
-         else if (gauge_position == 100 && rpm < rpm_UL) {
+         else if (gauge_position == 100 && rpm < rpm_UL || gauge_position != 0 && rpm < rpm_LL) {
              std::cout << "FAILED: gauge_position zu hoch, aber Drehzahl ist unter dem oberen Grenzwert!" << std::endl;
              ErrorCounter++; // Fehlerzähler erhöhen, wenn die gauge_position zu niedrig ist
          }
@@ -22,7 +23,7 @@ int main() {
              std::cout << "OK: gauge_position in Ordnung." << std::endl;
          }
     }
-    std::cout << "Anzahl der Fehler: " << ErrorCounter << std::endl;
+    std::cout <<"TESTS: " << testcounter << " FAILED: " << ErrorCounter << std::endl;
     if (ErrorCounter > 0) {
         std::cout << "Es gab Fehler bei der Durchflussüberwachung." << std::endl;
         return 1; // Fehlercode zurückgeben
