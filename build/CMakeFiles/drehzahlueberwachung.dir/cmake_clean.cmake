763f37a18522676cb7a03b946bcf3f09eb462941
wey-flefy <FFl@weycor.de>
2026-05-13 09:49:10 +0200

    build folder aus tracking entfernt

---
+++ build/CMakeFiles/drehzahlueberwachung.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drehzahlueberwachung.dir/src/drehzahlueberwachung.cpp.obj"
+  "CMakeFiles/drehzahlueberwachung.dir/src/drehzahlueberwachung.cpp.obj.d"
+  "libdrehzahlueberwachung.a"
+  "libdrehzahlueberwachung.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drehzahlueberwachung.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
