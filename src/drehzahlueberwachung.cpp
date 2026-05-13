54d075b8ac293eb6e35513833af0e7e1b0ab8ac8
wey-flefy <FFl@weycor.de>
2026-05-13 11:12:23 +0200

    Error Injection Tests should fail

--- src/drehzahlueberwachung.cpp
+++ src/drehzahlueberwachung.cpp
@@ -4,7 +4,7 @@
 bool drehzahlueberwachung(int drehzahl) {
     
     if (drehzahl > MAX_DREHZAHL) {
-        return 0; // Drehzahl zu hoch
+        return 1; // Drehzahl zu hoch
     }
-    return 1; // Drehzahl in Ordnung
+    return 0; // Drehzahl in Ordnung
 }
