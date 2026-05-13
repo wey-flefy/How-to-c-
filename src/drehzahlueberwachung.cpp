c33c5257584223d65d1c81581bf28de974902ec4
wey-flefy <FFl@weycor.de>
2026-05-13 11:14:14 +0200

    Tests i.O. ready to merge

--- src/drehzahlueberwachung.cpp
+++ src/drehzahlueberwachung.cpp
@@ -4,7 +4,7 @@
 bool drehzahlueberwachung(int drehzahl) {
     
     if (drehzahl > MAX_DREHZAHL) {
-        return 1; // Drehzahl zu hoch
+        return 0; // Drehzahl zu hoch
     }
-    return 0; // Drehzahl in Ordnung
+    return 1; // Drehzahl in Ordnung
 }
