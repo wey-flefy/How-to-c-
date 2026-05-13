941de657ebce1954af60745ba7ffca2cce573de9
wey-flefy <FFl@weycor.de>
2026-05-13 12:59:54 +0200

    V1.0.0

--- src/highflow.cpp
+++ src/highflow.cpp
@@ -3,10 +3,10 @@
 
 int highflow (int rpm) {
     
-    if (rpm < rpm_LL) {
+    if (rpm < rpm_LL) {     // Wenn die Drehzahl unter der unteren Grenze liegt, bleibt das ventil geschlossen (0% Durchfluss)
         return 0; 
     }
-    if (rpm > rpm_UL) {
+    if (rpm > rpm_UL) {    // Wenn die Drehzahl über der oberen Grenze liegt, bleibt das ventil vollständig geöffnet (100% Durchfluss)
         return 100; 
     }
     return (rpm - rpm_LL) * 100 / (rpm_UL - rpm_LL); // Berechnung der Durchflussrate in Prozent    
